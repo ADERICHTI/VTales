d86022fbff05da04e387139d5bf4e5762357f3e5
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-08-17 14:55:45 +0100

    Create main-db.js

---
+++ main-db.js
@@ -0,0 +1,33 @@
+
+// Stories Database
+const stories = {
+  "Lima's Door": {
+    logline: LimaLogline,
+    genre: "Fantasy",
+    imageUrl: "https://i.imgur.com/JQlEs9O.png",
+    chapters: [
+      {
+        number: 1,
+        title: "The Attic Discovery",
+        story: LimaChapter1
+      },
+      {
+        number: 2,
+        title: "Welcome to Oniria",
+        story: LimaChapter2
+      }
+    ]
+  },
+  "The Rat Taker": {
+    logline: TheRatTakerLogline,
+    genre: "Dark Fantasy",
+    imageUrl: "https://i.imgur.com/7VyvX3J.jpg",
+    chapters: [
+      {
+        number: 1,
+        title: "Plague and Shadows",
+        story: TheRatTakerChapter1
+      }
+    ]
+  }
+};
