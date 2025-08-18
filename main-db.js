60f6a42a8940c4359388bc52e68181a8376e36d6
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-08-18 22:21:56 +0100

    Update main-db.js

--- main-db.js
+++ main-db.js
@@ -1,32 +1,11 @@
 // Stories Database
 const stories = {
-  "Lima's Door": {
-    logline: LimaLogline,
-    genre: "Fantasy",
-    imageUrl: "https://raw.githubusercontent.com/ADERICHTI/Images001/bb69c5b6ab3160e243a75b0aab9a20cf4ec628d1/Screenshot_20250817_180812.png",
+  "Mech Hunter": {
+    logline: MechHunterLogline,
+    genre: "Sci-fi Horror",
+    imageUrl: "https://raw.githubusercontent.com/ADERICHTI/Images001/refs/heads/main/Mech%20Hunter.png",
     chapters: [
-      {
-        number: 1,
-        title: "The Attic Discovery",
-        story: LimaChapter1
-      },
-      {
-        number: 2,
-        title: "Welcome to Oniria",
-        story: LimaChapter2
-      }
-    ]
-  },
-  "The Rat Taker": {
-    logline: TheRatTakerLogline,
-    genre: "Dark Fantasy",
-    imageUrl: "https://raw.githubusercontent.com/ADERICHTI/Images001/refs/heads/main/Screenshot_20250817_182006.png",
-    chapters: [
-      {
-        number: 1,
-        title: "Plague and Shadows",
-        story: TheRatTakerChapter1
-      }
+      
     ]
   }
 };
