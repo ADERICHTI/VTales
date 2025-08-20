a933194f91260ca99417db345153831d1a8b4739
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-08-20 05:07:28 +0100

    Update firebase-auth.js

--- js/firebase-auth.js
+++ js/firebase-auth.js
@@ -20,6 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+window.app = app;
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
