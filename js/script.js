7286061adee652b2647206917ddbfab0d49f4f88
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-08-22 16:39:24 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -75,7 +75,7 @@ function renderStoryCards() {
     
     storyCard.innerHTML = `
       <div class="story-card-image" style="background-image: linear-gradient(hsl(${hslCode[0]}, ${hslCode[1]}%, ${hslCode[2]}%, 30%), hsl(${hslCode[0]}, ${hslCode[1]}%, ${hslCode[2]}%, 66%)), url('https://raw.githubusercontent.com/ADERICHTI/Images001/refs/heads/main/Mech%20Hunter.png');"></div>
-      <div class="story-card-content" style="background-color: ${color}; color: hslCode[3];">
+      <div class="story-card-content" style="background-color: ${color}; color: ${hslCode[3]};">
         <h3 class="story-card-title">${title}</h3>
         <p class="story-card-description">${storyData.logline}</p>
         <span class="story-card-genre">${storyData.genre || 'Adventure'}</span>
