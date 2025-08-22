3ca03f150a7efad22209b0f59e8c100c803cb189
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-08-22 15:21:28 +0100

    Update main-db.js

--- js/main-db.js
+++ js/main-db.js
@@ -2,7 +2,7 @@
 const stories = {
   "Mech Hunter": {
     logline: MechHunterLogline,
-    genre: "Sci-fi Horror",
+    genre: "sci-fi horror",
     imageUrl: "https://raw.githubusercontent.com/ADERICHTI/Images001/refs/heads/main/Mech%20Hunter.png",
     chapters: [
       { number: 1, title: "The Road's End", story: MechHunterChapter1 }
