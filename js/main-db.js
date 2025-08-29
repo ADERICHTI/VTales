920036752917b1627f913e055fcae32ff353d754
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-08-29 05:53:43 +0100

    Update main-db.js

--- js/main-db.js
+++ js/main-db.js
@@ -5,7 +5,7 @@ const stories = {
     genre: "Sci-fi Horror",
     imageUrl: "https://raw.githubusercontent.com/ADERICHTI/Images001/refs/heads/main/The%20Mech%20Hunter.png",
     chapters: [
-      { number: 1, title: "The Road's End", story: TheMechHunterChapter1 }
+      { number: 1, title: "The Road's End", story: TheMechHunterChapter1, imageUrl: "https://raw.githubusercontent.com/ADERICHTI/Images001/refs/heads/main/The%20Mech%20Hunter.png" }
     ]
   }
 };
